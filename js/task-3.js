3942290b2c91fa4c9b5c9d86675aea5b62e96375
ViktorIgnatiev <viktor.ignatiev@wp.pl>
2024-04-19 18:49:59 +0200

    Home Work VII/ save II / task 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,16 @@
 `use strict`
+
+const input = document.querySelector('#name-input')
+console.log(input);
+
+const span = document.querySelector('#name-output')
+console.log(span);
+
+input.addEventListener('input',(event) => {
+    if ( event.currentTarget.value.trim() === '') { 
+            span.textContent = 'Anonymous'
+    }
+    span.textContent = event.currentTarget.value;
+})
+
+
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,39 @@
 `use strict`
+
+const loginForm = document.querySelector('.login-form')
+
+
+
+
+const onFormFoo = event => {
+event.preventDefault();
+const emailInput = event.currentTarget.elements.email
+const passwordInput = event.currentTarget.elements.password
+
+if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+    alert('All form fields must be filled in')
+}else{
+    const data = new FormData(loginForm)
+    const formInfo = {}
+    data.forEach((newWalue, newKey) => {
+        formInfo[newKey] = newWalue.trim()  
+    });
+    
+    console.log(formInfo);
+    
+    event.currentTarget.reset();
+}
+}
+
+loginForm.addEventListener('submit', onFormFoo);
+
+
+// Альтернатива для виведення обєкту з данними форми
+
+// const formEl = event.currentTarget.elements;
+
+//   const info = {
+//     email: formEl.user_email.value,
+//     pass: formEl.user_password.value,
+//     message: formEl.user_comment.value,
+//   };
