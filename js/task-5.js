ed92ce0a60a9852cf964a56a37a3ff9366c0fd90
ViktorIgnatiev <viktor.ignatiev@wp.pl>
2024-04-19 22:16:21 +0200

    Home Work VII / save 3 / task 6 + CSS

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const spanTextColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body')
+
+const changeColorBtnFoo = () =>{
+  spanTextColor.textContent = getRandomHexColor()
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+
+changeColorBtn.addEventListener('click', changeColorBtnFoo)
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,43 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+input.classList.add('input-style');
+createBtn.classList.add('createBtn-style') 
+destroyBtn.classList.add('destroyBtn-style') 
+boxesContainer.classList.add('box-div-stile')
+
+function createBoxes(amount) {
+  destroyBoxes(); 
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = ''; 
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = input.value.trim();
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ''; 
+  }
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes()
+})
